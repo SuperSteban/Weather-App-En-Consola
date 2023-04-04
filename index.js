909faa03b5831a1e3526e00c3fcd76d96604bbc9
Jorge Esteban Santillan <38368851+SuperSteban@users.noreply.github.com>
2023-04-03 22:46:39 -0700

    models añadido, eslint cambio a extension json por problemas con Modulos Ecmascript modules

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-import { inquirerMenu, pausa } from './src/helpers/inquirer.js';
+import { inquirerMenu, leerInput, pausa } from './src/helpers/inquirer.js';
+// import { Busqueda } from './src/models/busquedas.js';
 
 const main = async () => {
+	// const busquedas = new Busqueda();
 	let opt;
 	do {
 		opt = await inquirerMenu();
-		console.log(opt);
+		let lugar = '';
+		switch(opt) {
+			case '1':
+				// mostrar mensaje
+				lugar = await leerInput('Cuidad: ');
+				console.log(lugar);
+				await pausa();
+				// buscar lugar
+				// seleccionar lugar
+				// mostrar resultado
+				console.log(`\nCuidad: \n`);
+				console.log(`\nLat: \n`);
+				console.log(`\nLng: \n`);
+				console.log(`\nTemperatura : \n`);
+				console.log(`\nTemperatura Max: \n`);
+				console.log(`\nTemperatura Min: \n`);
+
+
+				break;
+		}
 		if (opt !== 0) {
 			await pausa();
 		}
---
+++ src/models/busquedas.js
@@ -0,0 +1,13 @@
+export class Busqueda {
+	historial = [];
+
+	constructor() {
+		// TODO : Leer DB si existe
+        console.log(this.historial)
+	}
+
+	async cuidad(lugar = '') {
+		console.log(this.lugar);
+		return []; // TODO: regresar arreglo de cuidades;
+	}
+}
