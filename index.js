31599e8652c82d8c0ef34dfac956f031d08f05c9
Jorge Esteban Santillan <38368851+SuperSteban@users.noreply.github.com>
2023-04-05 23:20:32 -0700

    Metodo para consumir la api MAPBOX status 200, Nuevo.. paquete env y nueva variable de entorno

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import { inquirerMenu, leerInput, pausa } from './src/helpers/inquirer.js';
-// import { Busqueda } from './src/models/busquedas.js';
+import { Busqueda } from './src/models/busquedas.js';
+
 
 const main = async () => {
-	// const busquedas = new Busqueda();
+	const busquedas = new Busqueda();
 	let opt;
 	do {
 		opt = await inquirerMenu();
 		let lugar = '';
-		switch(opt) {
+		switch (opt) {
 			case '1':
 				// mostrar mensaje
 				lugar = await leerInput('Cuidad: ');
 				console.log(lugar);
 				await pausa();
 				// buscar lugar
+				await busquedas.cuidad(lugar);
 				// seleccionar lugar
 				// mostrar resultado
 				console.log(`\nCuidad: \n`);
@@ -22,7 +24,6 @@ const main = async () => {
 				console.log(`\nTemperatura : \n`);
 				console.log(`\nTemperatura Max: \n`);
 				console.log(`\nTemperatura Min: \n`);
-
 
 				break;
 		}
